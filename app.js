28a329a41be3634257112d552877d9d01805c5c9
alinaavelicu <127543226+alinaavelicu@users.noreply.github.com>
2023-03-23 21:30:43 +0200

    complete

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const progress = document.querySelector(".progress");
+/*const progress = document.querySelector(".progress");
 const loading = document.querySelector(".loading");
 
 let i = 0;
@@ -12,4 +12,23 @@ const interval = setInterval(() => {
         clearInterval(interval);
         loading.innerHTML = "Completed"
     }
+}, 1000);*/
+
+
+const circle = document.querySelector(".progress-circle");
+
+const loading = document.querySelector(".loading");
+let i = 0;
+const fakeUploadPerc = [0, 10 ,25, 40, 42, 60, 70, 75, 90, 100];
+
+const circumference = circle.getTotalLength();
+const interval = setInterval(() => {
+    circle.style.strokeDashoffset = circumference - (fakeUploadPerc[i] /100) * circumference;
+    loading.innerHTML = fakeUploadPerc[i] + "%"
+    if(i == fakeUploadPerc.length){
+        clearInterval(interval);
+        loading.innerHTML = "Done"
+    }
+i++;
+    
 }, 1000);
